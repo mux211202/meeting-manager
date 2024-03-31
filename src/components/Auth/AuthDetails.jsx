0f6e38a66e3884575e0579a5788f9f3c7ce51f67
mux2112 <maxjarinovsky@gmail.com>
2024-03-31 14:23:01 +0200

    removing the local storage on sign out

--- src/components/Auth/AuthDetails.jsx
+++ src/components/Auth/AuthDetails.jsx
@@ -1,10 +1,12 @@
 import { signOut } from "firebase/auth";
 import { auth } from "../../configs/firebaseConfig";
+import { USER_MEETINGS } from "../../configs/UserPageConfig";
 
 const AuthDetails = ({ authUser }) => {
   const userSignOut = () => {
     signOut(auth)
       .then(() => {
+        localStorage.removeItem(USER_MEETINGS);
         console.log("sign out successful");
       })
       .catch((error) => console.log(error));
